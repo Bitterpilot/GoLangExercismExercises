fafc0d6788b4bdc3c5d1e9cfdff16ceac65c08b3
Nathan Minissale <nathanminissale@gmail.com>
2018-11-21 19:29:09 +0800

    Bob Exercise: wip

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,31 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
 // Package bob should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+var isLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+
+// Hey takes in a remark and responds appropriatly
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	switch true {
+	case (strings.HasSuffix(remark, "?") && remark == strings.ToUpper(remark)):
+		// if you yell a question at him.
+		return "Calm down, I know what I'm doing!"
+	case strings.HasSuffix(remark, "?"):
+		// if you ask him a question.
+		return "Sure."
+	case (remark == strings.ToUpper(remark) && !(isLetter(remark))):
+		// if you yell at him.
+		return "Whoa, chill out!"
+	// case gibberish:
+	// if you address him without actually saying anything.
+	// return "Fine. Be that way!"
+	default:
+		// to anything else.
+		return "Whatever."
+	}
 }
