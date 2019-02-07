df6a08a9302f3b35c95ceedce6dd7fe1ec0a449f
Nathan Minissale <nathanminissale@gmail.com>
2019-02-07 14:11:59 +0800

    Hamming Exercise: initial solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,22 @@
 package hamming
 
+import "errors"
+
+// Distance tells if two stings have the same contents
+// returning the number of differences
 func Distance(a, b string) (int, error) {
+	switch {
+	case len(a) != len(b):
+		return 0, errors.New("lengths do not match")
+	default:
+		aS := []byte(a)
+		bS := []byte(b)
+		var count int
+		for i, v := range aS {
+			if v != bS[i]{
+				count++
+			}
+		}
+		return count, nil
+	}
 }
