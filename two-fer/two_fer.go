4e6bc74702294a8fd600975856184eda40354a09
Nathan Minissale <nathanminissale@gmail.com>
2019-02-07 12:13:31 +0800

    Two Fer Exercise: After Feedback
    Updated it to mush more simple

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,19 +1,14 @@
 // Package twofer has one function ShareWith
 package twofer
 
-import (
-	"strings"
-)
-
 // ShareWith takes a string and returns
 // One for x, one for me.
 // where
 // x = a string that is parsed into the function
 // empty string = "you,"
 func ShareWith(name string) string {
-	ul := []string{"One for", name + ",", "one for me."}
 	if name == "" {
-		ul[1] = "you,"
+		return "One for you, one for me."
 	}
-	return strings.Join(ul, " ")
+	return "One for " + name + ", one for me."
 }
