a99bdd6e23585e06eacb00d950aa270648dc684d
Nathan Minissale <nathanminissale@gmail.com>
2019-03-06 15:12:58 +0800

    RNA-Transcription: added comments

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,12 @@
+// Package strand implements functions to manipulate DNA strings
 package strand
 
+// ToRNA given a DNA strand, its transcribed RNA strand is formed
+// by replacing each nucleotide with its complement:
+//
+// G -> C
+// C -> G
+// T -> A
+// A -> U
 func ToRNA(dna string) string {
 }
