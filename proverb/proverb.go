46ef3f105212512c38f26d3c6fb0c5380404c678
Nathan Minissale <nathanminissale@gmail.com>
2019-03-06 11:26:47 +0800

    Proverb Exercise: passing tests

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,6 +1,8 @@
 // Package proverb writes proverbs
 package proverb
 
+import "fmt"
+
 // Proverb takes a slice of strings containing nouns for a proverb.
 // When parssing in the nouns "nail", "shoe" and "horse" the proverb is
 // returned as follows:
@@ -9,5 +11,19 @@ package proverb
 // "For want of a shoe the horse was lost.",
 // "And all for the want of a nail."
 func Proverb(rhyme []string) []string {
-	return []string{}
+	var proverb []string
+	if len(rhyme) == 0 {
+		return proverb
+	}
+	firstPiece := rhyme[0]
+	for i, piece := range rhyme {
+		if i != len(rhyme)-1 {
+			line := fmt.Sprintf("For want of a %s the %s was lost.", piece, rhyme[i+1])
+			proverb = append(proverb, line)
+		} else {
+			line := fmt.Sprintf("And all for the want of a %s.", firstPiece)
+			proverb = append(proverb, line)
+		}
+	}
+	return proverb
 }
