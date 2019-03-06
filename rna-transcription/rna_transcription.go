6b0b2f7b2181c5eaf81860a54a640416c0705c90
Nathan Minissale <nathanminissale@gmail.com>
2019-03-06 16:22:43 +0800

    RNA-Transcription: passing tests

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,6 +1,10 @@
 // Package strand implements functions to manipulate DNA strings
 package strand
 
+import (
+	"strings"
+)
+
 // ToRNA given a DNA strand, its transcribed RNA strand is formed
 // by replacing each nucleotide with its complement:
 //
@@ -9,4 +13,9 @@ package strand
 // T -> A
 // A -> U
 func ToRNA(dna string) string {
+	rna := strings.NewReplacer("G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U")
+	return rna.Replace(dna)
 }
